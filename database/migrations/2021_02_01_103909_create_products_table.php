4a739c32e39286b36ac457ea977fcde728cf4b3b
Jon Hogendorp <jonhoog@live.nl>
2021-02-02 09:34:12 +0100

    first commit

---
+++ database/migrations/2021_02_01_103909_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignKeyDefinition;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('food_name');
+            $table->enum('size', ['large', 'medium', 'small']);
+            $table->decimal('price', );
+
+            $table->unsignedBigInteger('food_type');
+            $table->unsignedBigInteger('restaurant_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
