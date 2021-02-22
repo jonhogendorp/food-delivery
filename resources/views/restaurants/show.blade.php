9c9ed8582557bbea535ac52d9cf35792607cd0d8
berthil <berthilbos@gmail.com>
2021-02-22 14:21:44 +0100

    show.blade followed Jon layout  + Berthil overlay

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -36,6 +36,7 @@
                             <h3>{{$product->food_name}}</h3>
                             <p>{{$product->price}}</p>
                             <p>ingredients</p>
+                            <p><button onclick="openNav()" class="button is-link">Order</button></p>
                         </li>
                 </div>
             </div>
@@ -43,114 +44,47 @@
         </div>
     </div>
     </section>
-    @endsection
-@section('content')
+    <div id="order" class="overlay">
 
-<section id="about" class="about">
-<!-- section-->
-  <section class="d-flex align-items-center">
-  </section>
-
-  <section id="why-us" class="why-us">
-
-  <div class="container" data-aos="fade-up">
-
-    <div class="section-title">
-      <h1 class="restauranttitle">{{$restaurant->restaurant_name}}</h1>
-    </div>
-        @foreach ($restaurant->products as $product)
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="box" data-aos="zoom-in" data-aos-delay="100">
-          <h4>{{$product->food_name}}</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus architecto eveniet adipisci voluptatum et maxime autem modi neque vel provident, veritatis id temporibus ut reiciendis rerum odit perspiciatis quia sunt.</p>
-          <h4>Price:</h4>
-          <p>{{$product->price}}</p>
-          <br>
-          
-          <button onclick="openNav()" class="button is-link">Order</button>
-        </div>
-      </div>
-    </div>  
-    
-      @endforeach
-  </div>
-
+      <!-- Button to close the overlay navigation -->
       
-  </section>
-
-</section>
-<div id="order" class="overlay">
-
-  <!-- Button to close the overlay navigation -->
-  
-
-  <!-- Overlay content -->
-  <div class="overlay-content">
-    <strong><h1 class="ordertitle">{{$restaurant->restaurant_name}}</h1></strong>
-    <h3 class="ordertitle">Your order:</h3>
-    <form method="POST" action="" class="orderoverlay" id="orderoverlay" name="orderoverlay" onsubmit="return false;">
-      @csrf
-      <div>
-        <div>
-          <label for="Product"></label>
-        <input type="text" class="product" name="product" id="product" value="{{$product->food_name}}" required>
-        </div>
-        <div>
-          <label for="size"></label>
-          <select name="size" id="size" class="size" required>
-            <option value="Small">Small</option>
-            <option value="Medium">Medium</option>
-            <option value="Large">Large</option>
-          </select>
-        </div>
-        <div>
-        <label for="price"></label>
-        <input type="text" class="price" name="price" id="price" value="{{$product->price}}" required>
-        <p>{{$errors->first('product')}}{{$errors->first('size')}}{{$errors->first('price')}}</p>
-        </div>
+    
+      <!-- Overlay content -->
+      <div class="overlay-content">
+        <strong><h1 class="ordertitle">{{$restaurant->restaurant_name}}</h1></strong>
+        <h3 class="ordertitle">Your order:</h3>
+        <form method="POST" action="" class="orderoverlay" id="orderoverlay" name="orderoverlay" onsubmit="return false;">
+          @csrf
+          <div>
+            <div>
+              <label for="Product"></label>
+            <input type="text" class="product" name="product" id="product" value="{{$product->food_name}}" required>
+            </div>
+            <div>
+              <label for="size"></label>
+              <select name="size" id="size" class="size" required>
+                <option value="Small">Small</option>
+                <option value="Medium">Medium</option>
+                <option value="Large">Large</option>
+              </select>
+            </div>
+            <div>
+            <label for="price"></label>
+            <input type="text" class="price" name="price" id="price" value="{{$product->price}}" required>
+            <p>{{$errors->first('product')}}{{$errors->first('size')}}{{$errors->first('price')}}</p>
+            </div>
+          </div>
+        
+        <button class="button is-link" type="submit">Confirm</button>
+        <button onclick="closeNav()" class="button is-link">Close</button>
+      </form>
+      
       </div>
     
-    <button class="button is-link" type="submit">Confirm</button>
-    <button onclick="closeNav()" class="button is-link">Close</button>
-  </form>
-  
-  </div>
-
-</div>
- 
-
-@endsection
-@section('content')
-<div class="row">
-<div class="col-lg-12 margin-tb m-5">
-<div class="pull-left">
-<h2> Show Product</h2>
-</div>
-<div class="pull-right">
-<a class="btn btn-primary" href="{{ route('restaurants.index') }}"> Back</a>
-</div>
-</div>
-</div>
-<div class="row">
-<div class="col-xs-12 col-sm-12 col-md-12">
-<div class="form-group">
-<strong>Restaurant Name:</strong>
-{{ $restaurant->restaurant_name }}
-</div>
-</div>
-<div class="col-xs-12 col-sm-12 col-md-12">
-<div class="form-group">
-<strong>Restaurant email:</strong>
-{{ $restaurant->email }}
-</div>
-</div>
-<div class="col-xs-12 col-sm-12 col-md-12">
-    <div class="form-group">
-    <strong>Restaurant phone:</strong>
-    {{ $restaurant->phone }}
     </div>
-    </div>
-</div>
-@endsection
-<p class="text-center text-primary"><small>Tutorial by rscoder.com</small></p>
+     
+    
+    @endsection
+
+
+    
